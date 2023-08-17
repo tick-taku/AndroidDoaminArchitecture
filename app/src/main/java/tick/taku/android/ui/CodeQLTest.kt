e0eed6031cbf993e6c3606f94ddb385b3e8ba7da
tick-taku <tick.taku.77+tech@gmail.com>
2023-08-17 15:58:57 +0900

    test

---
+++ app/src/main/java/tick/taku/android/ui/CodeQLTest.kt
@@ -0,0 +1,12 @@
+package tick.taku.android.ui
+
+import android.net.Uri
+
+class CodeQLTest {
+
+    fun test() {
+        Uri.parse("content://org.nextcloud/file")
+
+        val secretKey = "absfggkjsjhaduiw::"
+    }
+}
