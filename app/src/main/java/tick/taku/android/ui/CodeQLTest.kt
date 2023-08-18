b6ea8e6f1f29fbe908fbd3b9a4af0234b57aa1aa
tick-taku <tick.taku.77+tech@gmail.com>
2023-08-18 17:45:03 +0900

    test

--- app/src/main/java/tick/taku/android/ui/CodeQLTest.kt
+++ app/src/main/java/tick/taku/android/ui/CodeQLTest.kt
@@ -1,10 +1,15 @@
 package tick.taku.android.ui
 
+import android.content.Intent
 import android.net.Uri
 
 class CodeQLTest {
 
     fun test() {
+        val i = Intent()
+        i.setClassName("org.wordpress.android.prealpha", "org.wordpress.android.ui.posts.EditPostActivity")
+        i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+
         Uri.parse("content://org.nextcloud/file")
 
         val secretKey = "absfggkjsjhaduiw::"
