38e413ef9b190d68466e1460c0473d243520fceb
tick-taku <tick.taku.77+tech@gmail.com>
2023-08-22 02:06:29 +0900

    Adjust image size

--- feature/cat/src/main/java/tick/taku/android/feature/cat/compose/CatImages.kt
+++ feature/cat/src/main/java/tick/taku/android/feature/cat/compose/CatImages.kt
@@ -1,10 +1,14 @@
 package tick.taku.android.feature.cat.compose
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import tick.taku.android.core.entity.Cat
 
 @Composable
@@ -12,9 +16,16 @@ fun CatImages(
     cats: List<Cat>,
     modifier: Modifier = Modifier
 ) {
-    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(4.dp),
+        modifier = modifier
+    ) {
         items(cats) {
-            CatItem(cat = it)
+            CatItem(
+                cat = it,
+                modifier = Modifier.padding(4.dp).aspectRatio(0.8f)
+            )
         }
     }
 }
