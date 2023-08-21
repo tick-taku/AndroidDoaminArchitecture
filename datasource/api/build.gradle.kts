bdb6b7c49622749674e18ae5c5ba1034ea00ca23
tick-taku <tick.taku.77+tech@gmail.com>
2023-08-21 17:59:38 +0900

    Add datasource module

---
+++ datasource/api/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.gradle.library)
+    alias(libs.plugins.kotlin.gradle)
+    alias(libs.plugins.ksp.gradle)
+    alias(libs.plugins.dagger.hilt.gradle)
+}
+
+android {
+    namespace = "tick.taku.android"
+    compileSdk = libs.versions.target.sdk.get().toInt()
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            @Suppress("UnstableApiUsage")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core)
+    implementation(libs.coroutines.core)
+    implementation(libs.kotlinx.serialization)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.serialization.converter)
+    implementation(libs.okhttp)
+
+    implementation(libs.dagger.hilt)
+    ksp(libs.dagger.hilt.compiler)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "AndroidDomainArchitecture"
 include(":app")
+include(":datasource:api")
