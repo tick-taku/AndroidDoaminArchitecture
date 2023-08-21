256d3bf84e4f4910bd1d692739640295a7c244d6
tick-taku <tick.taku.77+tech@gmail.com>
2023-08-22 00:30:40 +0900

    Add :feature:cat module

---
+++ feature/cat/build.gradle.kts
@@ -0,0 +1,52 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.gradle.library)
+    alias(libs.plugins.kotlin.gradle)
+    alias(libs.plugins.ksp.gradle)
+    alias(libs.plugins.dagger.hilt.gradle)
+}
+
+android {
+    namespace = "tick.taku.android"
+    compileSdk = libs.versions.target.sdk.get().toInt()
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(project(":domain:cat"))
+    implementation(project(":core:entity"))
+
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.compose.ui.tooling)
+    implementation(libs.coroutines.core)
+
+    implementation(libs.dagger.hilt)
+    ksp(libs.dagger.hilt.compiler)
+}
