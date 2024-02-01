8d735633353960b7099aad4b288cfa92b0c8da24
tick-taku <tick.taku.77@gmail.com>
2024-02-01 10:31:41 +0900

    Make version up script

---
+++ bin/bump_version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+libs_file_path="gradle/libs.versions.toml"
+
+version="$1"
+versions=(${version//./ })
+
+sed -i -e "/versionMajor/s/.*/versionMajor = ${versions[0]}/g" $libs_file_path
+sed -i -e "/versionMinor/s/.*/versionMinor = ${versions[1]}/g" $libs_file_path
+sed -i -e "/versionPatch/s/.*/versionPatch = ${versions[2]}/g" $libs_file_path
+rm -rf "$libs_file_path-e"
