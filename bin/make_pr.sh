6859182835e3084d595e93fc3c4abd77116ec8b0
tick-taku <tick.taku.77@gmail.com>
2024-02-06 18:36:25 +0900

    template

--- bin/make_pr.sh
+++ bin/make_pr.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 new_version="$1"
-pr_numbers=$(gh pr list --json "milestone,number" -q "[.[] | select(.milestone.title == \"Release ${new_version}\") | .number] | map(\"- #\" + tostring)|join(\"\\\n\")")
-pr_body=$(cat .github/PULL_REQUEST/release.md | sed -e "s/# Release x\.x\.0/# Release ${new_version}/" | awk -v text="$pr_numbers" '/## Updates/{print;print text;next}1')
+release_title="Release ${new_version}"
+
+pr_numbers=$(gh pr list --json "milestone,number" -q "[.[] | select(.milestone.title == \"${release_title}\") | .number] | map(\"- #\" + tostring)|join(\"\\\n\")")
+pr_body=$(cat .github/PULL_REQUEST/release.md | sed -e "s/# Release x\.x\.0/# ${release_title}" | awk -v text="$pr_numbers" '/## Updates/{print;print text;next}1')
 
 gh pr create \
   -B main \
-  -t "Release $NEW_VERSION" \
+  -t "$release_title" \
+  -m "$release_title" \
   -b "$pr_body"
